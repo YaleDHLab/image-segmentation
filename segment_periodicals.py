c074d5a8083d813593d8bb99c8a9e2dcd3c495e3
duhaime <douglas.duhaime@gmail.com>
2016-07-17 22:40:39 -0400

    add code to segment articles into disparate images

--- segment_periodicals.py
+++ segment_periodicals.py
@@ -1,8 +1,9 @@
 from skimage import filters, segmentation, io
-from skimage.measure import label
+from skimage.measure import label, regionprops
 from skimage.color import label2rgb
 from scipy import ndimage
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import sys
 
 image_file = sys.argv[1]
@@ -48,7 +49,6 @@ clean_border = segmentation.clear_border(mask)
 plt.imshow(clean_border, cmap='gray')
 plt.show()
 
-
 #######################
 # Label image regions #
 #######################
@@ -58,6 +58,36 @@ image_label_overlay = label2rgb(labeled, image=im)
 
 fig, ax = plt.subplots(ncols=1, nrows=1, figsize=(6, 6))
 ax.imshow(image_label_overlay)
+#plt.show()
+
+#########################################
+# Draw bounding box around each article #
+#########################################
+
+# create array in which to store cropped articles
+cropped_images = []
+
+# define amount of padding to add to cropped image
+pad = 20
+
+for region_index, region in enumerate(regionprops(labeled)):
+  if region.area < 2000:
+    continue
+
+  # draw a rectangle around the segmented articles
+  # bbox describes: min_row, min_col, max_row, max_col
+  minr, minc, maxr, maxc = region.bbox
+  
+  # use those bounding box coordinates to crop the image
+  cropped_images.append(im[minr-pad:maxr+pad, minc-pad:maxc+pad])
+  
+  print "region", region_index, "bounding box:", minr, minc, maxr, maxc
+
+  rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
+                              fill=False, edgecolor='red', linewidth=2)
+
+  ax.add_patch(rect)
+
 plt.show()
 
 ###############
@@ -65,3 +95,6 @@ plt.show()
 ###############
 
 # can crop using: cropped = image_array[x1:x2,y1:y2]
+
+for c, cropped_image in enumerate(cropped_images):
+  io.imsave( str(c) + ".png", cropped_image)
